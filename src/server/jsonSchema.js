adf843c7b89d04f3dbd210de070faba8d6df5fa3
Jin wook Lee <leepc12@gmail.com>
2023-01-18 10:07:59 -0800

    json validator for igvf

--- src/server/jsonSchema.js
+++ src/server/jsonSchema.js
@@ -1,11 +1,14 @@
 const AjvDraft04 = require('ajv-draft-04');
-const Ajv = require('ajv');
+const AjvDraft202012 = require('ajv/dist/2020');
 
 global.validateJson = (schema, data) => {
+  // To use different schema engine for two platforms
+  // ENCODE: draft-4
+  // IGVF: draft-2022-12
   const ajv =
     schema.$schema === 'http://json-schema.org/draft-04/schema#'
       ? new AjvDraft04({ strict: false })
-      : new Ajv({ strict: false });
+      : new AjvDraft202012({ strict: false });
 
   const valid = ajv.validate(schema, data);
   return { valid, errors: ajv.errors };
