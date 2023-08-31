0e9c4b8dd9484d854603ecace881c202e43b59a6
Jin wook Lee <leepc12@gmail.com>
2023-08-31 10:42:31 -0700

    simplify menus

--- src/server/menu.js
+++ src/server/menu.js
@@ -2,14 +2,14 @@ global.onOpen = () => {
   const version = global.getScriptVersion();
   const menu = SpreadsheetApp.getUi().createMenu(`IGVF ${version}`);
 
-  menu.addItem('Check for update', 'checkForUpdate');
-  menu.addSeparator();
   menu.addItem('Search', 'search');
-  menu.addSeparator();
-  menu.addItem('Show sheet info', 'showSheetInfoAndHeaderLegend');
   menu.addItem('File upload sidebar', 'uploadSidebar');
   menu.addSeparator();
+  menu.addItem('Check for update', 'checkForUpdate');
+  menu.addItem('Show sheet info', 'showSheetInfoAndHeaderLegend');
+  menu.addSeparator();
   menu.addItem('Validate', 'validateJsonWithSchema');
+  menu.addItem('Export selected row to JSON', 'convertSelectedRowToJson');
   menu.addSeparator();
   menu.addItem('Make new template row', 'makeTemplateForUser');
   menu.addItem('GET metadata from portal', 'getMetadataForAllForUser');
@@ -19,50 +19,45 @@ global.onOpen = () => {
   // menu.addItem('PATCH-APPEND selected columns', 'patchAppendAll');
   // menu.addItem('PATCH-REMOVE selected columns', 'patchRemoveAll');
   menu.addSeparator();
-  menu.addItem('Make new template row (ADMIN ONLY)', 'makeTemplateForAdmin');
-  menu.addItem('GET metadata from portal (ADMIN ONLY)', 'getMetadataForAllForAdmin');
-  menu.addItem('PUT metadata to portal (ADMIN ONLY)', 'putAll');
-  menu.addSeparator();
-  menu.addItem('Export selected row to JSON', 'convertSelectedRowToJson');
-  menu.addSeparator();
 
   const submenuTools = SpreadsheetApp.getUi().createMenu('🛠 Tools');
-
+  submenuTools.addItem('Make new template row (ADMIN ONLY)', 'makeTemplateForAdmin');
+  submenuTools.addItem('GET metadata from portal (ADMIN ONLY)', 'getMetadataForAllForAdmin');
+  submenuTools.addItem('PUT metadata to portal (ADMIN ONLY)', 'putAll');
+  submenuTools.addSeparator();
   submenuTools.addItem('Open profile page', 'openProfilePage');
   submenuTools.addItem('Apply profile to sheet manually', 'applyProfileToSheet');
+  submenuTools.addSeparator();
   submenuTools.addItem('Export sheet to JSON', 'exportToJsonText');
   submenuTools.addItem('Export sheet to JSON file (Google Drive)', 'exportToJson');
-
+  submenuTools.addItem("Open tool's github page for README", 'openToolGithubPage');
+  const submenuDeveloper = SpreadsheetApp.getUi().createMenu('🛠 Developers only (for debugging)');
+  submenuDeveloper.addItem('Show current sheet developer metadata', 'showSheetAllDevMetadata');
+  submenuDeveloper.addItem('Show spreadsheet developer metadata', 'showSpreadsheetAllDevMetadata');
+  submenuDeveloper.addItem("Set current sheet's last used schema version", 'setLastUsedSchemaVersion');
+  submenuTools.addSubMenu(submenuDeveloper);
   menu.addSubMenu(submenuTools);
   menu.addSeparator();
 
   const submenuAuth = SpreadsheetApp.getUi().createMenu('⚙️ Authorization');
   // submenuAuth.addItem('Authorize for ENCODE', 'authorizeForEncode');
   submenuAuth.addItem('Authorize for IGVF', 'authorizeForIgvf');
-  submenuAuth.addItem('Authorize for AWS (for debugging)', 'authorizeForAws');
+  // submenuAuth.addItem('Authorize for AWS (for debugging)', 'authorizeForAws');
   menu.addSubMenu(submenuAuth);
 
   const submenuSettingsGlobal = SpreadsheetApp.getUi().createMenu('⚙️ Settings (Global)');
   submenuSettingsGlobal.addItem('Set default endpoint for READs (GET)', 'setDefaultEndpointRead');
   submenuSettingsGlobal.addItem('Set default endpoint for WRITEs (POST/PATCH/PUT)', 'setDefaultEndpointWrite');
+  submenuSettingsGlobal.addSeparator();
   submenuSettingsGlobal.addItem('Set default profile name', 'setDefaultProfileName');
   menu.addSubMenu(submenuSettingsGlobal);
 
   const submenuSettingsSheet = SpreadsheetApp.getUi().createMenu('⚙️ Settings (Current sheet)');
   submenuSettingsSheet.addItem('Set endpoint for READs (GET)', 'setEndpointRead');
   submenuSettingsSheet.addItem('Set endpoint for WRITEs (POST/PATCH/PUT)', 'setEndpointWrite');
+  submenuSettingsSheet.addSeparator();
   submenuSettingsSheet.addItem('Set profile name', 'setProfileName');
   menu.addSubMenu(submenuSettingsSheet);
-
-  menu.addSeparator();
-  menu.addItem("Open tool's github page for README", 'openToolGithubPage');
-  menu.addSeparator();
-
-  const submenuDeveloper = SpreadsheetApp.getUi().createMenu('🛠 Developer (for debugging)');
-  submenuDeveloper.addItem('Show current sheet developer metadata', 'showSheetAllDevMetadata');
-  submenuDeveloper.addItem('Show spreadsheet developer metadata', 'showSpreadsheetAllDevMetadata');
-  submenuDeveloper.addItem("Set current sheet's last used schema version", 'setLastUsedSchemaVersion');
-  menu.addSubMenu(submenuDeveloper);
 
   menu.addToUi();
 };
