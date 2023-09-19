91a14c5427fdb9b8523adac1b9adc3c91f62845d
Jin wook Lee <leepc12@gmail.com>
2023-09-19 15:16:00 -0700

    make menu simpler, endpoint(read/write) -> endpoint

--- src/server/menu.js
+++ src/server/menu.js
@@ -3,66 +3,55 @@ global.onOpen = () => {
   const menu = SpreadsheetApp.getUi().createMenu(`IGVF ${version}`);
 
   menu.addItem('Search', 'search');
-  menu.addItem('File upload sidebar', 'uploadSidebar');
   menu.addSeparator();
+  menu.addItem("Set profile name", 'setProfileName');
   menu.addItem('Show sheet info', 'showSheetInfoAndHeaderLegend');
-  menu.addSeparator();
-  menu.addItem('Check for script update', 'checkForUpdate');
-  menu.addSeparator();
-  menu.addItem('Update current sheet schema version', 'updateCurrentSheet');
-  // menu.addItem('Update all sheets', 'updateAllSheets');
   menu.addSeparator();
   menu.addItem('Validate', 'validateJsonWithSchema');
   menu.addSeparator();
-  menu.addItem('Export selected row to JSON', 'convertSelectedRowToJson');
-  menu.addSeparator();
-  menu.addItem('Make new template row', 'makeTemplateForUser');
   menu.addItem('GET metadata from portal', 'getMetadataForAllForUser');
   menu.addItem('POST new metadata to portal', 'postAll');
   menu.addItem('PATCH selected columns', 'patchSelected');
   menu.addItem('PATCH all columns', 'patchAll');
+  menu.addSeparator();
+  menu.addItem('Create a new sheet with updated profile schema', 'updateCurrentSheet');
+  menu.addSeparator();
+  menu.addItem('File upload sidebar', 'uploadSidebar');
+  menu.addSeparator();
+  menu.addItem('Export selected row to JSON', 'convertSelectedRowToJson');
   // menu.addItem('PATCH-APPEND selected columns', 'patchAppendAll');
   // menu.addItem('PATCH-REMOVE selected columns', 'patchRemoveAll');
   menu.addSeparator();
 
   const submenuTools = SpreadsheetApp.getUi().createMenu('🛠 Tools');
-  submenuTools.addItem('Make new template row (ADMIN ONLY)', 'makeTemplateForAdmin');
-  submenuTools.addItem('GET metadata from portal (ADMIN ONLY)', 'getMetadataForAllForAdmin');
-  submenuTools.addItem('PUT metadata to portal (ADMIN ONLY)', 'putAll');
+  submenuTools.addItem('Make a new template row', 'makeTemplateForUser');
+  submenuTools.addItem('Highlight sheet with profile schema', 'applyProfileToSheet');
   submenuTools.addSeparator();
   submenuTools.addItem('Open profile page', 'openProfilePage');
-  submenuTools.addItem('Apply profile to sheet manually', 'applyProfileToSheet');
   submenuTools.addSeparator();
   submenuTools.addItem('Export sheet to JSON', 'exportToJsonText');
   submenuTools.addItem('Export sheet to JSON file (Google Drive)', 'exportToJson');
-  submenuTools.addItem("Open tool's github page for README", 'openToolGithubPage');
+  submenuTools.addSeparator();
+  submenuTools.addItem('Make a new template row (ADMIN ONLY)', 'makeTemplateForAdmin');
+  submenuTools.addItem('GET metadata from portal (ADMIN ONLY)', 'getMetadataForAllForAdmin');
+  submenuTools.addItem('PUT metadata to portal (ADMIN ONLY)', 'putAll');
+
   const submenuDeveloper = SpreadsheetApp.getUi().createMenu('🛠 Developers only (for debugging)');
   submenuDeveloper.addItem('Show current sheet developer metadata', 'showSheetAllDevMetadata');
   submenuDeveloper.addItem('Show spreadsheet developer metadata', 'showSpreadsheetAllDevMetadata');
   submenuDeveloper.addItem("Set current sheet's last used schema version", 'setLastUsedSchemaVersion');
+  submenuDeveloper.addSeparator();
   submenuDeveloper.addItem('Authorize for ENCODE', 'authorizeForEncode');
-  // submenuDeveloper.addItem('Test Google Drive', 'testGoogleDrive');
   submenuTools.addSubMenu(submenuDeveloper);
+
   menu.addSubMenu(submenuTools);
   menu.addSeparator();
 
-  const submenuAuth = SpreadsheetApp.getUi().createMenu('⚙️ Authorization');
-  submenuAuth.addItem('Authorize for IGVF', 'authorizeForIgvf');
-  menu.addSubMenu(submenuAuth);
-
-  const submenuSettingsGlobal = SpreadsheetApp.getUi().createMenu('⚙️ Settings (Global)');
-  submenuSettingsGlobal.addItem('Set default endpoint for READs (GET)', 'setDefaultEndpointRead');
-  submenuSettingsGlobal.addItem('Set default endpoint for WRITEs (POST/PATCH/PUT)', 'setDefaultEndpointWrite');
-  submenuSettingsGlobal.addSeparator();
-  submenuSettingsGlobal.addItem('Set default profile name', 'setDefaultProfileName');
-  menu.addSubMenu(submenuSettingsGlobal);
-
-  const submenuSettingsSheet = SpreadsheetApp.getUi().createMenu('⚙️ Settings (Current sheet)');
-  submenuSettingsSheet.addItem('Set endpoint for READs (GET)', 'setEndpointRead');
-  submenuSettingsSheet.addItem('Set endpoint for WRITEs (POST/PATCH/PUT)', 'setEndpointWrite');
-  submenuSettingsSheet.addSeparator();
-  submenuSettingsSheet.addItem('Set profile name', 'setProfileName');
-  menu.addSubMenu(submenuSettingsSheet);
+  menu.addItem('Authorize for IGVF', 'authorizeForIgvf');
+  menu.addItem('Set endpoint', 'setDefaultEndpoint');
+  menu.addSeparator();
+  menu.addItem('Check for script update', 'checkForUpdate');
+  menu.addItem("Open tool's github page for README", 'openToolGithubPage');
 
   menu.addToUi();
 };
