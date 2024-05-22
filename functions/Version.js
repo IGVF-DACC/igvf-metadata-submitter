d4eb45e165fbb1881952386fa184917e443d62c4
Mingjie Li <mingjiecn@gmail.com>
2024-05-22 15:26:50 -0500

    move update.md

--- functions/Version.js
+++ functions/Version.js
@@ -1,7 +1,7 @@
 SCRIPT_VERSION='v0.3.3';
 URL_LATEST_SCRIPT_VERSION='https://api.github.com/repos/igvf-dacc/igvf-metadata-submitter/releases/latest';
 URL_PREFIX_UPDATE_HELP='https://github.com/IGVF-DACC/igvf-metadata-submitter/blob/';
-URL_SUFFIX_UPDATE_HELP='docs/UPDATE.md';
+URL_SUFFIX_UPDATE_HELP='/UPDATE.md';
 
 
 function getScriptVersion() {
